e6be7ae6a9d8b2b3adae2ebc97e55f8ee26eb8bc
Everton <imevertonrch@gmail.com>
2023-04-18 16:03:32 -0300

    Criando card de video dinamicamente

--- js/showVideos.js
+++ js/showVideos.js
@@ -2,22 +2,22 @@ import { getVideos } from "./apiConnect.js";
 
 const lista = document.querySelector("[data-video]");
 
-const createCard = function () {
+const createCard = function ({ id, titulo, descricao, url, imagem }) {
+  console.log(id, titulo, descricao, url, imagem);
   const listItemVideo = document.createElement("li");
   listItemVideo.classList.add("videos__item");
   listItemVideo.innerHTML = `
         <li class="videos__item">
-            <iframe width="100%" height="72%" src="https://www.youtube.com/embed/YhnNOTde2I0"
-                title="Mercado de Trabalho | Desmistificando Mobile - Episódio 5" frameborder="0"
+            <iframe width="100%" height="72%" src="${url}"
+                title="${titulo}"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen></iframe>
             <div class="descricao-video">
-                <img src="./img/logo.png" alt="logo canal alura">
-                <h3>Mercado de Trabalho | Desmistificando Mobile...</h3>
-                <p>1,1 mil visualizações</p>
+                <img src="${imagem}" alt="logo canal alura">
+                <h3>${titulo}</h3>
+                <p>${descricao}</p>
             </div>
-        </li>    
-    `;
+        </li> `;
   return listItemVideo;
 };
 
@@ -25,7 +25,10 @@ const videos = async function () {
   try {
     const data = await getVideos.json();
     if (!data) throw new Error(`Json error (${data})`);
+
+    data.forEach((video) => lista.appendChild(createCard(video)));
   } catch (error) {
     console.error(error);
   }
 };
+videos();
